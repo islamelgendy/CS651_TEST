dfb8bd3933db56c68f62cb2782353c1196ce14c6
islamelgendy <47329391+islamelgendy@users.noreply.github.com>
2019-08-08 18:13:34 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS651_TEST.Default" %>
+
+<!DOCTYPE html>
+
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head runat="server">
+	<title>CS651-Test1-Question2a</title>
+	</head>
+	<body>
+	<form id="form1" runat="server">
+	<div>    
+	<asp:Label ID="Label1" runat="server" Text="Enter the number of random numbers to be generated"></asp:Label>
+	<asp:TextBox ID="inputTextBox" runat="server"></asp:TextBox>
+	<br />
+	<asp:Button ID="submitButton" runat="server" Text="Submit" />
+	<br />
+	<asp:Label ID="Label2" runat="server" Text="The generated numbers" Visible="True"></asp:Label>
+	<br />
+	<asp:ListBox ID="outputListBox" runat="server" Rows="10" Visible="False">
+	</asp:ListBox>
+	<br />
+	<asp:Label ID="Label3" runat="server" Text="Range of generated numbers" Visible="False"></asp:Label>
+	<asp:TextBox ID="rangeTextBox" runat="server" Visible="False"></asp:TextBox>    
+	</div>
+	</form>
+	</body>
+	</html>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS651_TEST
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                inputTextBox.Visible = false;
+                Label1.Visible = false;
+                outputListBox.Visible = true;
+                submitButton.Visible = false;
+                Label2.Visible = true;
+                Label3.Visible = true;
+                rangeTextBox.Visible = true;
+                int num = Int32.Parse(inputTextBox.Text);
+                generateNums(num);
+            }
+        }
+        private void generateNums(int num)
+        {
+            int[] arr = new int[num];
+            Random number = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = number.Next(1, 100);
+                outputListBox.Items.Add(arr[i].ToString());
+            }
+            int min = arr[0];
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            rangeTextBox.Text = min + " - " + max;
+        }
+
+    }
+}
